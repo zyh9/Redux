0688e810ff50f494c0a1d13b8771371d0a64432c
zhangyanhui <1124596031@qq.com>
2018-02-13 16:41:42 +0800

    hui

--- ShopCart-demo/src/index.js
+++ ShopCart-demo/src/index.js
@@ -5,40 +5,37 @@ import './index.css';
 import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 // 引入方法，重新封装reducer
-// import {persistStore, persistCombineReducers} from 'redux-persist';
-// import { PersistGate } from 'redux-persist/es/integration/react';
-// import storage from 'redux-persist/es/storage';
+import {persistStore, persistReducer} from 'redux-persist';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import storage from 'redux-persist/es/storage';
 
-// const config = {key: 'redux',storage};
+const appReducer = combineReducers({
+  reducers
+})
+const config = {key: 'redux',storage};
 
-// function configureStore(){
-//   let reducer = persistCombineReducers(config, reducers);
-//   let store = createStore(reducer, applyMiddleware(thunk));
-//   let persistor = persistStore(store);
-//   return { persistor, store }
-// }
-
-// const { persistor, store } = configureStore()
+const persistedReducer = persistReducer(config, appReducer);
 
 // 创建 store
-let store = createStore(
-  reducers,
+let store = compose(
   applyMiddleware(thunk),
   // redux启用调试工具
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+)(createStore)(persistedReducer)
+
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <PersistGate persistor={persistor}> */}
+    <PersistGate persistor={persistor}>
       <App />
-    {/* </PersistGate> */}
+    </PersistGate>
   </Provider>
   , document.getElementById('root')
-);
+)
 registerServiceWorker();
